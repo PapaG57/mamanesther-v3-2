faf92b15f1dd24b2db806765cfe4f631c2679c03
Florent GERARD <florentgerard@sfr.fr>
2023-05-20 16:53:46 +0200

    ajout d'un lien vidéo sur les flags + refonte code

---
+++ assets/js/video.js
@@ -0,0 +1,16 @@
+var playVideo = document.getElementById("playVideo");
+var video = document.getElementById("myVideo");
+var loadingSpinner = document.getElementById("loadingSpinner");
+
+playVideo.addEventListener("click", function() {
+    playVideo.style.display = "none";
+    video.style.display = "block";
+    video.play();
+});
+
+video.addEventListener("ended", function() {
+    loadingSpinner.style.display = "block";
+    setTimeout(function() {
+        window.location.href = "accueil.html"; // Remplacez l'URL par celle de la page web vers laquelle vous souhaitez être redirigé.
+    }, 2000); // 2 secondes de délai avant la redirection
+});
